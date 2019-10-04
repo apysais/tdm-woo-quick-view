0b761c180578a4e5bb3c211bea6945349f8ddaad
allan_paul_casilum <allan.paul.casilum@gmail.com>
2019-10-04 15:08:05 +0800

    update function name

--- functions/woo.php
+++ functions/woo.php
@@ -4,20 +4,20 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-if ( ! function_exists( 'mru_quickview_before_item' ) ) {
+if ( ! function_exists( 'wqv_quickview_before_item' ) ) {
 
-    function mru_quickview_before_item() { ?>
+    function wqv_quickview_before_item() { ?>
       <div class="quick-view-container">
         <button type="button" class="close close-quick-view hidden" aria-label="Close"><span class="close-button" aria-hidden="true">&times;</span></button>
     <?php }
 }
+add_action( 'woocommerce_before_shop_loop_item', 'wqv_quickview_before_item', 10);
 
-add_action( 'woocommerce_before_shop_loop_item', 'mru_quickview_before_item', 10);
-if ( ! function_exists( 'mru_quickview_after_item' ) ) {
-    function mru_quickview_after_item() {
+if ( ! function_exists( 'wqv_quickview_after_item' ) ) {
+    function wqv_quickview_after_item() {
         global $product;
 
-        //$see_more = get_field('detail_page', $product->get_id());
+        //$see_more = get_post_meta('detail_page', $product->get_id());
         $see_more = '';
 
         if($see_more && $see_more == 1) { ?>
@@ -32,8 +32,7 @@ if ( ! function_exists( 'mru_quickview_after_item' ) ) {
       </div>
     <?php }
 }
-
-add_action( 'woocommerce_after_shop_loop_item', 'mru_quickview_after_item', 10);
+add_action( 'woocommerce_after_shop_loop_item', 'wqv_quickview_after_item', 10);
 
 add_action( 'wcqv_product_summary', 'woocommerce_template_single_price', 15 );
 add_action( 'wcqv_product_summary', 'woocommerce_template_single_add_to_cart', 25 );
